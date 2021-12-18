33d192908c4367360bf01279802b106f8a97675f
云金杞 <yunjinqi@qq.com>
2021-12-18 11:59:24 +0000

    update setup.py.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description="pyfolio packages modified by yunjinqi",
     long_description_content_type = "text/markdown",
     url="https://gitee.com/yunjinqi/pyfolio",
-    packages=['pyfolio'],
+    packages='pyfolio',
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3.6",
