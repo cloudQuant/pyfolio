79e692c42c524ce06219590068cb9fd443cd097a
cloudQuant <yunjinqi@gmail.com>
2025-01-07 21:48:40 +0800

    fix install shell code

--- install_package.sh
+++ install_package.sh
@@ -9,3 +9,12 @@ pip install -U ./pyfolio
 # 切换回 pyfolio 目录
 cd pyfolio
 
+# 如果存在pyfolio.egg-info文件夹，删除之
+if [ -d "pyfolio.egg-info" ]; then
+    rm -rf pyfolio.egg-info
+fi
+# 如果存在build文件夹，删除之
+if [ -d "build" ]; then
+    rm -rf build
+fi
+
